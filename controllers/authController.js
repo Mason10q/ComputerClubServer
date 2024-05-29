c4936106bf7401bc921fe927a228767ac3e8a696
mason10q <moskvichev610@gmail.com>
2024-05-29 12:04:25 +0300

    initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const crypto = require('node:crypto');
+const dotenv = require('dotenv');
+
+function getConfig() {
+    return dotenv.config({path: __approot + '/.env'});
+}
+
+function getDb() {
+    let config = getConfig();
+    return mysql.createConnection({
+        host: config.parsed.DB_HOST,
+        user: config.parsed.DB_USER,
+        database: config.parsed.DB_NAME,
+        password: config.parsed.DB_PASS
+    });
+}
---
+++ routers/mainRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mainRouter = express.Router();
+const urlEncodedParser = express.urlencoded({extended: false});
+
+const authController = require("../controllers/authController.js");
+const mainController = require("../controllers/mainController.js");
+    
+
+
+module.exports = mainRouter;
